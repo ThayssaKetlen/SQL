25d824a6dced1d1eacd3d21056ee7e336a44f1ee
thayssa <est.thayssa.889@gmail.com>
2023-01-06 16:28:00 -0300

    adicionei os selcts

--- inicio.sql
+++ inicio.sql
@@ -1,1 +1,5 @@
 SHOW DATABASES -para mostrar todos os banco de dados existentes
+
+SELECT * FROM <nome do tabela> - para listar as colunas e os dados da tabela
+
+SELECT <nome da coluna> FROM <nome da tabela> - para listar os dados da coluna escolhida de certa tabela
