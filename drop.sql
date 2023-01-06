26f6d9e94b2cbb4ef6d0b67d96b56db54cb07697
thayssa <est.thayssa.889@gmail.com>
2023-01-06 17:00:49 -0300

    acrescentei coisas

---
+++ drop.sql
@@ -0,0 +1,4 @@
+DROP DATABASE <nome do banco de dados> - para excluir o banco de dados
+
+DROP TABLE <nome da tabela> -para excluir uma tabela (selecione o banco de dados primeiro)
+
--- inicio.sql
+++ inicio.sql
@@ -1,5 +1,11 @@
+USE <nome do banco de dados> - para entrar dentro do banco selecionado
+
 SHOW DATABASES -para mostrar todos os banco de dados existentes
+
+SHOW TABLES - para listar as tabelas existentes
 
 SELECT * FROM <nome do tabela> - para listar as colunas e os dados da tabela
 
 SELECT <nome da coluna> FROM <nome da tabela> - para listar os dados da coluna escolhida de certa tabela
+
+DESCRIBE <nome da tabela> - para listar os componentes da tabela selecionada
