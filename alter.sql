ed047160ae650a952207e5f5083b5cef7d6682cd
thayssa <est.thayssa.889@gmail.com>
2023-01-10 16:13:08 -0300

    comandos alter

---
+++ alter.sql
@@ -0,0 +1,15 @@
+ALTER TABLE <nome da tabela> ADD <nome da coluna> (tipo de dado) ; - serve para adicionar uma coluna a uma certa tabela
+
+ALTER TABLE <nome da tabela> DROP <nome da coluna> ; -serve para apagar uma coluna de uma certa tabela
+
+ALTER TABLE <nome da tabela> ADD <nome da coluna> (tipo de dado) AFTER <nome da coluna> - serve para especificar o lugar que vc quer q a coluna adicionada esteja
+
+ALTER TABLE <nome da tabela> ADD <nome da coluna> (tipo de dado) FIRST - serve para adiconar a coluna como primeiro campo
+
+ALTER TABLE <nome da tabela> MODIFY <nome da coluna> (tipo de dado) - serve para modificar as definições da coluna
+
+ALTER TABLE <nome da tabela> CHANGE COLUMN  <nome atual da coluna> <nome novo> tipo de dado) - serve para renomear a coluna
+
+ALTER TABLE <nome atual da tabela > RENAME TO <nome futuro> - renomear  a tabela
+
+ALTER TABLE <nome da tabela> ADD PRIMARY KEY(<>nome da coluna desejada)
